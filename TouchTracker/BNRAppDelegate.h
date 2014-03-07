a3e859ce66d809b82cb3ebc831823bd9cd99f34c
Justin Steffen <JustinDSN@gmail.com>
2014-03-06 16:05:55 -0800

    Initial Commit

---
+++ TouchTracker/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  TouchTracker
+//
+//  Created by Justin Steffen on 3/6/14.
+//  Copyright (c) 2014 Justin Steffen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TouchTracker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TouchTracker
+//
+//  Created by Justin Steffen on 3/6/14.
+//  Copyright (c) 2014 Justin Steffen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
